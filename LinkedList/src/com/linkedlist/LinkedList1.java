f8777ff9b8560b7b4af1464bac2b86b6c596e619
milton <milton@gmail.com>
2019-05-26 17:37:14 +0530

    2 problems

---
+++ LinkedList/src/com/linkedlist/LinkedList1.java
@@ -0,0 +1,104 @@
+package com.linkedlist;
+
+public class LinkedList1 {
+
+	Node head;
+
+	class Node {
+		int data;
+		Node next;
+
+		Node(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+
+	public LinkedList1 insertion(LinkedList1 list, int data) {
+
+		Node new_node = new Node(data);
+		new_node.next = null;
+
+		if (list.head == null) {
+			list.head = new_node;
+		} else {
+			Node current = list.head;
+			while (current.next != null) {
+				current = current.next;
+			}
+			current.next = new_node;
+		}
+		return list;
+	}
+
+	// getMiddle Node
+	public int getMiddle(LinkedList1 list) {
+
+		int middle_data = 0;
+		Node fist_pointer = list.head;
+		Node slow_pointer = this.head;
+
+		while (fist_pointer != null && fist_pointer.next != null) {
+			fist_pointer = fist_pointer.next.next;
+			slow_pointer = slow_pointer.next;
+		}
+		middle_data = slow_pointer.data;
+		return middle_data;
+
+	}
+
+	public int getNodeFromLast(LinkedList1 list, int position) {
+		int data= 0;
+		int length=0;
+		int count=0;
+		Node current = this.head;
+		while(current.next != null) {
+			length++;
+			current = current.next;
+		}
+
+		length = (length-position)+1;
+		Node curr = this.head;
+		while(curr.next !=null) {
+			count++;
+			curr= curr.next;
+			if(length == count) {
+				data = curr.data;
+			}
+		}
+		
+		return data;
+		
+	}
+	
+	public void print() {
+		Node current = this.head;
+		while (current != null) {
+			System.out.print(current.data + "->");
+			current = current.next;
+		}
+	}
+
+	public static void main(String[] args) {
+
+		LinkedList1 list = new LinkedList1();
+		list = list.insertion(list, 1);
+		list = list.insertion(list, 2);
+		list = list.insertion(list, 3);
+		list = list.insertion(list, 4);
+		list = list.insertion(list, 5);
+		list = list.insertion(list, 6);
+		list = list.insertion(list, 7);
+		list = list.insertion(list, 8);
+		list = list.insertion(list, 9);
+		list = list.insertion(list, 10);
+		// list = list.insertion(list,11);
+		list.print();
+
+		int midData = list.getMiddle(list);
+		System.out.println("\nMiddle data: " + midData);
+		int dataFromLast=list.getNodeFromLast(list, 3);
+		System.out.println("Node from last: "+dataFromLast);
+		
+	}
+}
